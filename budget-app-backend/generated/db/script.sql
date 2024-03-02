df1a2d27683ec0539defbcb747906e9bdc0e8d86
sameerajayasoma <sameera.madushan@gmail.com>
2024-03-01 17:05:58 -0800

    Add the generated directory

---
+++ budget-app-backend/generated/db/script.sql
@@ -0,0 +1,35 @@
+-- AUTO-GENERATED FILE.
+
+-- This file is an auto-generated file by Ballerina persistence layer for model.
+-- Please verify the generated scripts and execute them against the target DB server.
+
+DROP TABLE IF EXISTS `CategoryBudget`;
+DROP TABLE IF EXISTS `ExpenseItem`;
+DROP TABLE IF EXISTS `ExpenseCategory`;
+
+CREATE TABLE `ExpenseCategory` (
+	`id` VARCHAR(191) NOT NULL,
+	`name` VARCHAR(191) NOT NULL,
+	`description` VARCHAR(191) NOT NULL,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `ExpenseItem` (
+	`id` VARCHAR(191) NOT NULL,
+	`description` VARCHAR(191) NOT NULL,
+	`amount` DECIMAL(65,30) NOT NULL,
+	`date` VARCHAR(191) NOT NULL,
+	`categoryId` VARCHAR(191) NOT NULL,
+	FOREIGN KEY(`categoryId`) REFERENCES `ExpenseCategory`(`id`),
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `CategoryBudget` (
+	`id` VARCHAR(191) NOT NULL,
+	`amount` DECIMAL(65,30) NOT NULL,
+	`year` VARCHAR(191) NOT NULL,
+	`month` VARCHAR(191) NOT NULL,
+	`categorybudgetId` VARCHAR(191) UNIQUE NOT NULL,
+	FOREIGN KEY(`categorybudgetId`) REFERENCES `ExpenseCategory`(`id`),
+	PRIMARY KEY(`id`)
+);
